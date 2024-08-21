8c3f169c6dd956113a4279fd4fca42125a64c2d0
Marina Rogova <marirog86@gmail.com>
2024-08-21 19:52:31 -0300

    precios rounded y opcion cancelar

--- js/main.js
+++ js/main.js
@@ -16,7 +16,8 @@ function elegirProducto(){
             2. Iphone 15
             3. Iphone 14
             4. Smartwatch
-            5. Samsung Galaxy S24`);
+            5. Samsung Galaxy S24
+            6. Cancelar compra`);
             switch(producto) {
                     case "1":
                         nombreProducto="Iphone 15 Pro";
@@ -53,6 +54,10 @@ function elegirProducto(){
                         productoValido=true;
                         console.log(`Agregaste ${nombreProducto} al carrito. Precio U$S ${precio}`)
                         break;
+                    case "6":
+                        seguirComprando=false;
+                        productoValido=false;
+                        break;
                     default:
                         console.log("Producto Inválido");
                         productoValido=false;
@@ -78,7 +83,7 @@ function elegirProducto(){
                 }
             }while (!opcionValida);
     }while (seguirComprando);
-    
+    return producto;
 }
 
 function elegirPlanPago(){
@@ -98,19 +103,19 @@ function elegirPlanPago(){
                     cantCuotas="en 3 cuotas";
                     precioFinal *=1.05;
                     opcionValida=true;
-                    console.log(`Elegiste pagar ${cantCuotas}. Precio final U$S ${precioFinal}`);
+                    console.log(`Precio final U$S ${precioFinal.toFixed(2)} ${cantCuotas} de U$S ${(precioFinal/3).toFixed(2)}`);
                     break;
                 case "3": 
                     cantCuotas="en 6 cuotas";
                     precioFinal *=1.1;
                     opcionValida=true;
-                    console.log(`Elegiste pagar ${cantCuotas}. Precio final U$S ${precioFinal}`);
+                    console.log(`Precio final U$S ${precioFinal.toFixed(2)} ${cantCuotas} de U$S ${(precioFinal/6).toFixed(2)}`);
                     break;
                 case "4":  
                     cantCuotas="en 12 cuotas";
                     precioFinal *=1.15;
                     opcionValida=true;
-                    console.log(`Elegiste pagar ${cantCuotas}. Precio final U$S ${precioFinal}`);
+                    console.log(`Precio final U$S ${precioFinal.toFixed(2)} ${cantCuotas} de U$S ${(precioFinal/12).toFixed(2)}`);
                     break;
                 default:
                     console.log("Opción Inválida");
@@ -119,5 +124,10 @@ function elegirPlanPago(){
     }while (!opcionValida)    
 }
 
-elegirProducto();
-elegirPlanPago();
+compra=elegirProducto();
+
+if (compra!="6"){
+    elegirPlanPago();
+}else{
+    console.log("Compra cancelada")
+}
