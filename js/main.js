c8a2d06abb9c8e9307ef0bccb317542ea9de101a
Marina Rogova <marirog86@gmail.com>
2024-08-21 17:56:08 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,115 @@
+let producto;
+let nombreProducto;
+let cantCuotas;
+let seguirComprando=true;
+let precio=0;
+let precioFinal=precio;
+let planPago;
+let productoValido=true;
+let opcionValida=true;
+let seguirComprandoString;
+do{
+    producto = prompt(`Selecciona un producto: 
+        1. Iphone 15 Pro  
+        2. Iphone 15
+        3. Iphone 14
+        4. Smartwatch
+        5. Samsung Galaxy S24`);
+        switch(producto) {
+                case "1":
+                    nombreProducto="Iphone 15 Pro";
+                    precio=1200;
+                    precioFinal +=precio;
+                    productoValido=true;
+                    console.log(`Agregaste ${nombreProducto} al carrito. Precio U$S ${precio}`)
+                    break;
+                case "2":
+                    nombreProducto="Iphone 15";
+                    precio=1000;
+                    precioFinal +=precio;
+                    productoValido=true;
+                    console.log(`Agregaste ${nombreProducto} al carrito. Precio U$S ${precio}`)
+                    break;
+                case "3":
+                    nombreProducto="Iphone 14";
+                    precio=800;
+                    precioFinal +=precio;
+                    productoValido=true;
+                    console.log(`Agregaste ${nombreProducto} al carrito. Precio U$S ${precio}`)
+                    break;
+                case "4":
+                    nombreProducto="Smartwatch";
+                    precio=200;
+                    precioFinal +=precio;
+                    productoValido=true;
+                    console.log(`Agregaste ${nombreProducto} al carrito. Precio U$S ${precio}`)
+                    break;
+                case "5":
+                    nombreProducto="Samsung Galaxy S24";
+                    precio=1100;
+                    precioFinal +=precio;
+                    productoValido=true;
+                    console.log(`Agregaste ${nombreProducto} al carrito. Precio U$S ${precio}`)
+                    break;
+                default:
+                    console.log("Producto Inválido");
+                    productoValido=false;
+        }
+        do {
+            if (productoValido){
+                seguirComprandoString=prompt(`
+                    1. Seguir comprando
+                    2. Finalizar compra`)
+                    switch(seguirComprandoString) {
+                        case "1":
+                            seguirComprando=true;
+                            opcionValida=true;
+                            break;
+                        case "2": 
+                            seguirComprando=false;
+                            opcionValida=true;
+                            break;
+                        default:
+                            console.log("Opción Inválida");
+                            opcionValida=false;
+                }
+            }
+        }while (!opcionValida);
+}while (seguirComprando);
+
+do{
+    planPago = prompt(`Selecciona plan de pagos:
+        1. Contado 
+        2. 3 cuotas (5% recargo)
+        3. 6 cuotas (10% recargo)
+        4. 12 cuotas (15% recargo)`);
+        switch(planPago) {
+            case "1":
+                cantCuotas="Contado";
+                opcionValida=true;
+                console.log(`Elegiste pagar ${cantCuotas}. Precio final U$S ${precioFinal}`);
+                break;
+            case "2": 
+                cantCuotas="en 3 cuotas";
+                precioFinal *=1.05;
+                opcionValida=true;
+                console.log(`Elegiste pagar ${cantCuotas}. Precio final U$S ${precioFinal}`);
+                break;
+            case "3": 
+                cantCuotas="en 6 cuotas";
+                precioFinal *=1.1;
+                opcionValida=true;
+                console.log(`Elegiste pagar ${cantCuotas}. Precio final U$S ${precioFinal}`);
+                break;
+            case "4":  
+                cantCuotas="en 12 cuotas";
+                precioFinal *=1.15;
+                opcionValida=true;
+                console.log(`Elegiste pagar ${cantCuotas}. Precio final U$S ${precioFinal}`);
+                break;
+            default:
+                console.log("Opción Inválida");
+                opcionValida=false;
+        }
+}while (!opcionValida)
+
