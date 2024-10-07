39160397e87f76ecad309e54fb9c69f74867b300
Marina Rogova <marirog86@gmail.com>
2024-10-07 17:05:42 -0300

    cambios

--- js/carrito.js
+++ js/carrito.js
@@ -8,28 +8,6 @@ const botonBorrarFiltro = document.querySelector(".btn-borrar-filtro");
 
 
 
-/*class Producto {
-    constructor(nombre, precio, categoria, id, imagen, cantidad) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.categoria = categoria;
-        this.id = id;
-        this.imagen = imagen;
-        this.cantidad = cantidad;
-    }
-}
-
-const producto1 = new Producto("Iphone 15 Pro", 1200, "Smartphone", "producto1", "./img/apple-iphone15-pro.jpg", 0);
-const producto2 = new Producto("Iphone 15", 1000, "Smartphone", "producto2", "./img/apple-iphone15.jpg", 0);
-const producto3 = new Producto("Iphone 14 Pro", 900, "Smartphone", "producto3", "./img/iPhone14-pro.jpg", 0);
-const producto4 = new Producto("Iphone 14", 800, "Smartphone", "producto4", "./img/iPhone14.jpg", 0);
-const producto5 = new Producto("Samsung Galaxy S24", 1100, "Smartphone", "producto5", "./img/samsung-galaxy-s24.jpg", 0);
-const producto6 = new Producto("Smartwatch 1", 200, "Smartwatch", "producto6", "./img/samsung-galaxy-fit-3.webp", 0);
-const producto7 = new Producto("Smartwatch 2", 250, "Smartwatch", "producto7", "./img/reloj-smartwatch-samsung-galaxy-watch-7.webp", 0);
-const producto8 = new Producto("Smartwatch 3", 150, "Smartwatch", "producto8", "./img/samsung-galaxy-watch-6.webp", 0);
-const producto9 = new Producto("Smartwatch 4", 300, "Smartwatch", "producto9", "./img/apple-watch-series-9.webp", 0);
-const producto10 = new Producto("Smartwatch 5", 100, "Smartwatch", "producto10", "./img/apple-watch-ultra.webp", 0);*/
-
 
 let catalogo = [];
 
@@ -37,6 +15,7 @@ fetch("../datos.json")
     .then((respuesta) => respuesta.json())
     .then((datos) => {
         catalogo = datos;
+        mostrarCatalogo(catalogo, seccionProductos);
     })
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
